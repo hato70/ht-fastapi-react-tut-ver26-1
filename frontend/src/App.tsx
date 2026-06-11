c9c95777fb67b5951123c8c2caafe649eebcd51c
hato70 <yaladres@web.de>
2026-06-11 22:28:39 +0200

    changed App.tsx and Todos.tsx from internet Abdulazeez page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { defaultSystem } from "@chakra-ui/react"
 import Header from "./components/Header";
+import Todos from "./components/Todos"; 
 
 function App() {
 
   return (
     <ChakraProvider value={defaultSystem}>
       <Header />
+<Todos />
     </ChakraProvider>
   )
 }
